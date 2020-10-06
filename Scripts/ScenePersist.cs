baff632c650dd74718b8f595506b88f5ca098a2d
Nikhil Kumar <nikilkumar0810@gmail.com>
2020-10-07 01:45:41 +0530

    Enable data persist in a scene. Add a rising water feature. Completed the section and course.

--- Scripts/ScenePersist.cs
+++ Scripts/ScenePersist.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScenePersist : MonoBehaviour
 {
+    int sceneIndex;
+
+    private void Awake()
+    {
+        int numberOfSession = FindObjectsOfType<ScenePersist>().Length;
+        if (numberOfSession > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != sceneIndex) {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Scripts/VerticalScoll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScoll : MonoBehaviour
+{
+    [SerializeField] float scrollSpeed = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(0,scrollSpeed * Time.deltaTime));
+    }
+}
